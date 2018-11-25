1f5acffd60874b2c7d44ca766b2c1f44a308aaa3
marabesi <matheus.marabesi@gmail.com>
2018-11-25 13:21:50 +0100

    Started implementation
    - Added env.json to ignored files
    - Removed unused logo.svg file
    - created firebase initial config (authentication)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebaseui from 'firebaseui';
+import firebase from 'firebase';
+import env from './env.json';
+
+const firebaseOauth = firebase.auth;
+
+console.log(firebaseOauth)
+
+const uiConfig = {
+    signInSuccessUrl: '<url-to-redirect-to-on-success>',
+    signInOptions: [
+        firebaseOauth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    ],
+
+    tosUrl: '<your-tos-url>',
+    privacyPolicyUrl: function () {
+        window.location.assign('<your-privacy-policy-url>');
+    }
+};
+
 
 class App extends Component {
+
+  constructor(...args) {
+    super(...args);
+
+    firebase.initializeApp(env);
+  }
+
+  componentDidMount() {
+
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
   render() {
+
     return (
       <div className="App">
+        <div id="firebaseui-auth-container"></div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
