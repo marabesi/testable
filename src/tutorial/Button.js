d9c072c4f5264822bbad591e19472113bc8ee516
marabesi <matheus.marabesi@gmail.com>
2019-02-10 20:30:22 +0100

    enabled className through props and fix identation

--- src/tutorial/Button.js
+++ src/tutorial/Button.js
@@ -4,9 +4,10 @@ import './button.scss';
 
 export default class Button extends Component {
 
-    render() {
-        return (
-            <button className="button">{this.props.description}</button>
-        );
-    }
+  render() {
+    const className = `button outline-none ${this.props.className ? this.props.className: ''}`;
+    return (
+      <button className={className}>{this.props.description}</button>
+    );
+  }
 }
