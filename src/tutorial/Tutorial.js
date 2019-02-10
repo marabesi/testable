196c60dc55c89a343d252e7579191cbf8b57e874
marabesi <matheus.marabesi@gmail.com>
2019-02-10 22:48:54 +0100

    changed identation from 4 spaces to 2

--- src/tutorial/Tutorial.js
+++ src/tutorial/Tutorial.js
@@ -6,15 +6,15 @@ import './tutorial.scss';
 
 export default class Tutorial extends Component {
 
-    render() {
-        return (
-            <div className="tutorial flex">
-                <Helmet>
-                    <style>{'body { background-color: #520F87; }'}</style>
-                </Helmet>
+  render() {
+    return (
+      <div className="tutorial flex">
+        <Helmet>
+          <style>{'body { background-color: #520F87; }'}</style>
+        </Helmet>
 
-                <Scene text="first step" button="Ok" step="1" className="m-auto w-4/5"/>
-            </div>
-        );
-    }
+        <Scene text="first step" button="Ok" step="1" className="m-auto w-4/5"/>
+      </div>
+    );
+  }
 }
