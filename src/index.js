aef908fdacbc7466610ad6abb122e5fba51f9e64
marabesi <matheus.marabesi@gmail.com>
2019-02-23 19:31:29 +0100

    added react-ga package

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import './tailwind.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-135081264-1');
+}
 
 ReactDOM.render(
   <BrowserRouter basename={process.env.REACT_APP_BASE_NAME}>
