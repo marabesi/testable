8b511c5dac6a9eaba96508914275da71ec51c88d
marabesi <matheus.marabesi@gmail.com>
2019-02-24 22:28:49 +0100

    minor changes to level component to fit design

--- src/level/Level.js
+++ src/level/Level.js
@@ -6,9 +6,9 @@ export default class Level extends Component {
 
     render() {
         return (
-            <div className="level py-3">
-                <h1 className="title">level {this.props.level}</h1>
-                <div className="progress-holder py-1 px-2">
+            <div className="flex level py-3">
+                <h1 className="title mr-3 text-white">level {this.props.level}</h1>
+                <div className="progress-holder py-1 px-2 h-8">
                     <div className={'progress py-2 progress-' + this.props.progress}></div>
                 </div>
             </div>
