f21c87549f5aec546bdfa013bb50835d152166db
marabesi <matheus.marabesi@gmail.com>
2019-04-12 20:45:19 +0200

    level component with es6 template syntax

--- src/components/level/Level.js
+++ src/components/level/Level.js
@@ -8,7 +8,7 @@ export default class Level extends Component {
       <div className="flex level py-3">
         <h1 className="title mr-3 text-white">level {this.props.level}</h1>
         <div className="progress-holder py-1 px-2 h-8">
-          <div className={"progress py-2 progress-" + this.props.progress} />
+          <div className={`progress py-2 progress-${this.props.progress}`} />
         </div>
       </div>
     );
