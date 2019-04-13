787a4d171aeadbd87d41a8f7411d7ccb6e86d925
marabesi <matheus.marabesi@gmail.com>
2019-04-13 21:02:10 +0200

    updated tutorial json file reference

--- src/components/introduction/Tutorial.js
+++ src/components/introduction/Tutorial.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import Scene from './Scene';
-import content from '../tutorial-content.json';
+import content from '../introduction-content.json';
 
 import './tutorial.scss';
 
