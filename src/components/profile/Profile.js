d307bde076e70fc9bf22c6fee562c99b716cfcbc
marabesi <matheus.marabesi@gmail.com>
2019-04-14 20:49:34 +0200

    added logout option

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { fakeAuth } from '../../pages/login/Auth';
 
 import './profile.scss';
 
 export default class Profile extends Component {
+
+  state = {
+    menu: false,
+    successfullLoggedOut: false
+  }
+
+  constructor(){
+    super();
+
+    this.showMenu = this.showMenu.bind(this);
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  onLogout() {
+    fakeAuth.signout(() => {
+      this.setState({
+        successfullLoggedOut: true,
+        menu: false
+      });
+    })
+  }
+
+  showMenu() {
+    this.setState({
+      menu: !this.state.menu
+    })
+  }
+
   render() {
+    if (this.state.successfullLoggedOut) {
+      return (
+        <Redirect to={{
+          pathname: '/'
+        }} />
+      );
+    }
+
     const name = this.props.user.name;
-    const className = `profile ${
+    const className = `profile relative cursor-pointer ${
       this.props.className ? this.props.className : ''
     }`;
     return (
-      <div className={className}>
+      <div className={className} onClick={this.showMenu}>
         <div className="picture-holder">
           <img
             src="https://placeimg.com/200/200/any"
@@ -22,6 +60,10 @@ export default class Profile extends Component {
             {name}
           </h2>
         </div>
+
+        <ul className={`absolute mt-12 w-full bg-testable-overlay list-reset p-2 z-20 ${this.state.menu ? 'block' : 'hidden'}`}>
+          <li className="cursor-pointer text-white" onClick={this.onLogout}>Logout</li>
+        </ul>
       </div>
     );
   }
