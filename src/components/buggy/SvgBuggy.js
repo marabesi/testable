da7cc52a0171e9bfcf176e364c6838a3e1233c85
marabesi <matheus.marabesi@gmail.com>
2019-04-14 15:04:18 +0200

    added alt t o svg buggy component to prevent eslint error

--- src/components/buggy/SvgBuggy.js
+++ src/components/buggy/SvgBuggy.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const SvgBuggy = props => (
-  <img src="assets/buggy.png" {...props} />
+  <img src="assets/buggy.png" {...props} alt="buggy" />
 );
 export default SvgBuggy;
 
