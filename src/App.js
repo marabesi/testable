3e78de7cd034de9df27f62705e8b5d665f165b7c
marabesi <matheus.marabesi@gmail.com>
2019-04-17 20:40:00 +0200

    enabled animation when transitioning via react router

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from './pages/login/Login';
 import Introduction from './pages/introduction/Introduction';
 import Tutorial from './pages/tutorial/Tutorial';
 import ProtectedRoute from './pages/login/router/ProtectedRoute';
 import NotFound from './pages/notfound/NotFound';
+import { spring, AnimatedSwitch } from 'react-router-transition';
 
 import './app.scss';
 
+function mapStyles(styles) {
+  return {
+    opacity: styles.opacity,
+    transform: `scale(${styles.scale})`,
+  };
+}
+
+function bounce(val) {
+  return spring(val, {
+    stiffness: 330,
+    damping: 42,
+  });
+}
+
+const bounceTransition = {
+  atEnter: {
+    opacity: 0,
+    scale: 1.2,
+  },
+  atLeave: {
+    opacity: bounce(0),
+    scale: bounce(0.8),
+  },
+  atActive: {
+    opacity: bounce(1),
+    scale: 1,
+  }
+};
 class App extends Component {
 
   render() {
     return (
       <React.Fragment>
-        <div className="App">
-          <div className="stars"></div>
-          <Switch>
-            <Route exact path="/" component={Login} />
-            <ProtectedRoute path="/intro" component={Introduction} />
-            <ProtectedRoute path="/tutorial" component={Tutorial} />
-            <Route path="*" component={NotFound} />
-          </Switch>
-        </div>
+        <AnimatedSwitch
+          atEnter={bounceTransition.atEnter}
+          atLeave={bounceTransition.atLeave}
+          atActive={bounceTransition.atActive}
+          mapStyles={mapStyles}
+          className="App"
+        >
+          <Route exact path="/" component={Login} />
+          <ProtectedRoute path="/intro" component={Introduction} />
+          <ProtectedRoute path="/tutorial" component={Tutorial} />
+          <Route path="*" component={NotFound} />
+        </AnimatedSwitch>
       </React.Fragment>
     );
   }
