80ab883964f69f93f494febecd3a58e92dc74e7d
marabesi <matheus.marabesi@gmail.com>
2019-04-18 19:06:58 +0200

    eslint fixes and profile picture forced width

--- src/pages/login/router/ProtectedRoute.js
+++ src/pages/login/router/ProtectedRoute.js
@@ -4,24 +4,24 @@ import { fakeAuth } from '../Auth';
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
   return (
-  <Route
-    {...rest}
-    render={(props) => {
-      const redirectTo = fakeAuth.canEnter(props.location)
-      return !redirectTo ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{
-            pathname: redirectTo,
-            state: { from: props.location }
-          }}
-        />
-      )
-    }
-  }
-  />
-)
+    <Route
+      {...rest}
+      render={(props) => {
+        const redirectTo = fakeAuth.canEnter(props.location);
+        return !redirectTo ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: redirectTo,
+              state: { from: props.location }
+            }}
+          />
+        );
+      }
+      }
+    />
+  );
 };
 
 export default ProtectedRoute;
