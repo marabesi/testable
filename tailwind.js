8bd5ae70c6ba69e005ec64ab86268d7c7964dea7
marabesi <matheus.marabesi@gmail.com>
2019-04-24 21:40:36 +0200

    changed blue-dark tailwind color

--- tailwind.js
+++ tailwind.js
@@ -98,7 +98,7 @@ let colors = {
 
   'blue-darkest': '#12283a',
   'blue-darker': '#1c3d5a',
-  'blue-dark': '#2779bd',
+  'blue-dark': '#012345',
   'blue': '#3490dc',
   'blue-light': '#6cb2eb',
   'blue-lighter': '#bcdefa',
