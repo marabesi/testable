39d264c221ac66691111272b7b77573cf521461e
marabesi <matheus.marabesi@gmail.com>
2019-05-11 18:27:41 +0200

    added level up animation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,12 +3,39 @@ import Profile from '../../components/profile/Profile';
 import Level from '../../components/level/Level';
 import DebugButton from '../../components/debug/Button';
 import { auth } from '../../pages/login/Auth';
+import Emitter, { LEVEL_UP } from '../../emitter/Emitter';
 
 export default class Header extends Component {
+
+  state = { 
+    levelup: false
+  };
 
   constructor() {
     super();
     this.goToIntroduction = this.goToIntroduction.bind(this);
+  }
+
+  resetLevelUpAnimation() {
+    setTimeout(() => {
+      this.setState({
+        levelup: false
+      });
+    }, 500);
+  }
+  componentDidMount() {
+    Emitter.addListener(LEVEL_UP, (data) => {
+      this.setState({
+        levelup: true
+      });
+
+      const level = data.increase + auth.user.level;
+      auth.updateUserInfo({
+        level: level
+      });
+
+      this.resetLevelUpAnimation();
+    });
   }
 
   goToIntroduction() {
@@ -25,7 +52,7 @@ export default class Header extends Component {
         <DebugButton onClick={this.goToIntroduction} value="go back to introduction"/>
 
         <div className="flex justify-between pl-3 pr-3 mt-3">
-          <div className="user-progress">
+          <div className={ `user-progress  ${this.state.levelup ? 'wobble-ver-right' : ''}`}>
             <Level progress={50} level={auth.user.level} />
           </div>
 
