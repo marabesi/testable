e08e327d385d93f2570a7f0a8b2b52e28a206241
marabesi <matheus.marabesi@gmail.com>
2019-05-19 11:58:10 +0200

    added loading spec file

---
+++ src/components/loading/Loading.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Loading from './Loading';
+import { shallow } from 'enzyme';
+
+describe('Loading component', () => {
+
+  it('should render loading component', () => {
+    const wrapper = shallow(<Loading />);
+
+    expect(wrapper.find('h1').text()).toContain('Loading...');
+  });
+});
