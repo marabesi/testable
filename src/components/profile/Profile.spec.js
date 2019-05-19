af3d93ac729b558778c6250ef04bb7285fd95ba0
marabesi <matheus.marabesi@gmail.com>
2019-05-19 11:57:54 +0200

    improved profile test suite

--- src/components/profile/Profile.spec.js
+++ src/components/profile/Profile.spec.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Profile from './Profile';
 import { shallow } from 'enzyme';
+import { auth } from '../../pages/login/Auth';
+
+const userData = { name: 'fake user', email: 'fake@john.com' };
 
 describe('profile component', () => {
   it('should place image from placeimg when user image does not exists', () => {
@@ -18,9 +21,48 @@ describe('profile component', () => {
   });
 
   it('should have name and email on title property', () => {
-    const wrapper = shallow(<Profile user={{ name: 'john', email: 'john@john.com' }} />);
+    const wrapper = shallow(<Profile user={userData} />);
     const container = wrapper.find('div');
 
-    expect(container.at(0).prop('title')).toEqual('john - john@john.com'); 
+    expect(container.at(0).prop('title')).toEqual('fake user - fake@john.com'); 
+  });
+
+  it('should logout', () => {
+    auth.signout = (cb) => { cb(); };
+    const wrapper = shallow(
+      <Profile
+        className="menu-wrapper"
+        user={userData}
+      />
+    );
+
+    const container = wrapper.find('.menu-wrapper');
+
+    container.at(0).simulate('click');
+    wrapper.find('ul li').simulate('click');
+
+    expect(wrapper.state().successfullLoggedOut).toBeTruthy();
+    expect(wrapper.state().menu).toBeFalsy();
+  });
+
+  it('should close menu on blur', () => {
+    const wrapper = shallow(
+      <Profile
+        className="menu-wrapper"
+        user={userData}
+      />
+    );
+
+    const container = wrapper.find('.menu-wrapper');
+
+    expect(wrapper.state().menu).toBeFalsy();
+
+    container.at(0).simulate('click');
+
+    expect(wrapper.state().menu).toBeTruthy();
+
+    container.simulate('click');
+
+    expect(wrapper.state().menu).toBeFalsy();
   });
 });
