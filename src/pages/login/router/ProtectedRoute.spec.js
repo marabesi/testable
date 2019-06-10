15602dcd0bf19b364e5e129037ef94127c90f4aa
marabesi <matheus.marabesi@gmail.com>
2019-06-10 08:44:11 +0200

    added protected route component test file

---
+++ src/pages/login/router/ProtectedRoute.spec.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ProtectedRoute from './ProtectedRoute';
+import { BrowserRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+import { auth } from '../Auth';
+
+describe('protected route component', () => {
+
+  test('should render component when allowed', () => {
+    auth.canEnter = (history, location) => {
+      return {
+        flag: true
+      };
+    };
+
+    const MyComponent = (props) => {
+      return (
+        <h1>my component</h1>
+      );
+    };
+
+    const wrapper = mount(
+      <BrowserRouter>
+        <ProtectedRoute
+          component={MyComponent}
+        />
+      </BrowserRouter>
+    );
+
+    expect(wrapper.find('h1').length).toEqual(1);
+    expect(wrapper.find('h1').text()).toEqual('my component');
+  });
+});
