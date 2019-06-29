a75271e6f49574825de92773c56c824f5caf90b9
marabesi <matheus.marabesi@gmail.com>
2019-06-29 18:51:29 +0200

    added link to logo component

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Base64Image from '../base64image/Base64Image';
 
 const Logo = props => (
-  <Base64Image
-    image="testable.logo.png"
-    alt="logo"
-    title="Testable - Ferramenta gamificada" {...props}
-  />
+  <a href="https://github.com/marabesi/testable" target="_blank">
+    <Base64Image
+      image="testable.logo.png"
+      alt="logo"
+      title="Testable - Ferramenta gamificada" {...props}
+    />
+  </a>
 );
 export default Logo;
