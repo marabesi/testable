f65eaf6302d07cb4fc2ff3fda5c60d6a9bbd69b1
marabesi <matheus.marabesi@gmail.com>
2019-07-23 20:02:35 +0200

    added rocket-01 page (first exercise after tutorting the user)

---
+++ src/pages/rocket-01/Rocket01.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router';
+import tddContent from './rocket01-guide-content';
+import EditorManager from '../../components/editor-manager/EditorManager';
+import Guide from '../../components/editor-manager/Guide';
+import Emitter, { PROGRESS_UP, LEVEL_UP } from '../../emitter/Emitter';
+import { track } from '../../emitter/Tracking';
+import { auth } from '../login/Auth';
+import { SumBehavior } from '../../engine/strategies/behavior/SumBehavior';
+import Reason from '../../engine/Reason';
+
+const code = `function somar(a, b) {
+  return a + b
+}`;
+
+const test = `function testeSomarNumerosPositivos() {
+}`;
+
+export default class Rocket01 extends Component {
+
+  state = {
+    code: {
+      0: code,
+      1: test
+    },
+    done: false,
+    currentExercise: 0,
+    showNext: false,
+    currentHint: 0,
+    initialStep: 0,
+    introEnabled: false,
+  };
+
+  componentDidMount() {
+    track({
+      section: 'tdd',
+      action: 'tdd_start'
+    });
+  }
+
+  onValidCode = (code, i) => {
+    if (this.state.currentHint !== 1) {
+      return;
+    }
+
+    let current = Object.assign({}, this.state.code);
+
+    current[i] = code;
+
+    this.setState({
+      ...this.state.code, code: current
+    });
+
+    if (Reason(code, SumBehavior)) {
+      Emitter.emit(LEVEL_UP);
+
+      track({
+        section: 'tdd',
+        action: 'tdd:valid_code',
+        value: code
+      });
+
+      this.setState({
+        //@ts-ignore
+        ...this.state.currentHint, currentHint: this.state.currentHint + 1,
+        ...this.state.showNext, showNext: false
+      });
+    }
+  }
+
+  onGuideFinishedTyping = () => {
+    // if (this.state.currentHint === 1) {
+    //   return;
+    // }
+
+    this.setState({
+      //@ts-ignore
+      ...this.state.showNext, showNext: true
+    });
+  }
+
+  handleProgress = () => {
+    const next = this.state.currentHint + 1;
+    const total = tddContent.length;
+    const isNotLast = next < total;
+
+    if (isNotLast) {
+      this.setState({
+        //@ts-ignore
+        ...this.state.currentHint, currentHint: next,
+        ...this.state.showNext, showNext: false
+      });
+
+      Emitter.emit(PROGRESS_UP, { amount: auth.user.progress + 10 });
+
+      track({
+        section: 'tdd',
+        action: 'next_guide_hint:button_click',
+        value: next
+      });
+
+      return;
+    }
+
+    this.setState({
+      //@ts-ignore
+      ...this.state.done, done: true
+    });
+  }
+
+  render() {
+    if (this.state.done) {
+      return (<Redirect to="/completed" />);
+    }
+
+    return (
+      <React.Fragment>
+        <div className="flex flex-col">
+          <div className="flex justify-center editor-container">
+            <EditorManager
+              editor={2}
+              className="w-2/5 m-5"
+              code={this.state.code}
+              onValidCode={{ 0: this.onValidCode, 1: this.onValidCode }}
+            />
+          </div>
+
+          <Guide
+            guideContent={tddContent}
+            showNext={this.state.showNext}
+            handleProgress={this.handleProgress}
+            currentHint={this.state.currentHint}
+            onFinishedTyping={this.onGuideFinishedTyping}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/pages/rocket-01/rocket01-guide-content.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    line: 'O foguete que temos que construir, é feito de dois sitemas princpais...',
+    style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'O primeiro que vamos trabalhar aqui são os motores...',
+    style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'Para que os motores funcionem precisamos escrever uma função de teste...',
+    style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'E essa função deve verificar se a cadeira de caracteres está ao contrário',
+    style: 'text-white font-semibold text-xl text-blue-light'
+  },
+];
