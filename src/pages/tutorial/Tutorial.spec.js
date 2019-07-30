9c8e0b0b7cc5bf8dbe790607f4a17b3615c47c4e
marabesi <matheus.marabesi@gmail.com>
2019-07-30 09:29:53 +0200

    added tutorial page spec test

--- src/pages/tutorial/Tutorial.spec.js
+++ src/pages/tutorial/Tutorial.spec.js
@@ -1,12 +1,66 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { Tutorial } from './Tutorial';
+import Emitter, { LEVEL_UP } from '../../emitter/Emitter';
+
+const ENABLE_EDITOR_ON_HINT = 3;
 
 describe('Tutorial page', () => {
+  afterEach(() => {
+    Emitter.removeAllListeners(LEVEL_UP);
+  });
+
   test('renders without crashing', () => {
     const wrapper = shallow(<Tutorial />);
 
     expect(wrapper.find('Intro').length).toBe(1);
     expect(wrapper.find('EditorManager').length).toBe(1);
   });
+
+  test.each([
+    0,
+    1,
+    2,
+  ])('should not enable editor before the allowed hint - current hint %s', (currentHint) => {
+    const wrapper = shallow(<Tutorial />);
+    wrapper.instance().setState({
+      currentHint
+    });
+
+    expect(wrapper.instance().onValidCode('')).toBe(undefined);
+  });
+
+  test('level up on valid code', () => {
+    const callback = jest.fn();
+    Emitter.addListener(LEVEL_UP, callback);
+
+    const wrapper = shallow(<Tutorial />);
+    wrapper.instance().setState({
+      currentHint: ENABLE_EDITOR_ON_HINT
+    });
+    wrapper.instance().onValidCode('function somar(a,b) {return a + b}');
+
+    expect(callback).toBeCalled();
+  });
+
+  test('No action on invalid code provided', () => {
+    const callback = jest.fn();
+    Emitter.addListener(LEVEL_UP, callback);
+
+    const wrapper = shallow(<Tutorial />);
+    wrapper.instance().setState({
+      currentHint: ENABLE_EDITOR_ON_HINT
+    });
+    wrapper.instance().onValidCode('function somar() {}');
+
+    expect(callback).toBeCalledTimes(0);
+  });
+
+  test('Enable tooltip', () => {
+    const wrapper = shallow(<Tutorial />);
+    wrapper.instance().onEnableTooltip();
+
+    expect(wrapper.instance().state.introEnabled).toBe(true);
+  });
+  
 });
