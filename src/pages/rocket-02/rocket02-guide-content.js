eb18ed3eb88ec4330dd635b4fad6d32150bb2bb6
marabesi <matheus.marabesi@gmail.com>
2019-08-04 20:04:23 +0200

    minor improvements

--- src/pages/rocket-02/rocket02-guide-content.js
+++ src/pages/rocket-02/rocket02-guide-content.js
@@ -6,5 +6,9 @@ export default [
   {
     line: 'rocket 02 -2',
     style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'rocket 02 -3',
+    style: 'text-white font-semibold text-xl text-blue-light'
   }
 ];
--- src/pages/rocket-03/rocket03-guide-content.js
+++ src/pages/rocket-03/rocket03-guide-content.js
@@ -2,5 +2,13 @@ export default [
   {
     line: 'rocket 03',
     style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'rocket 03-02',
+    style: 'text-white font-semibold text-xl text-blue-light'
+  },
+  {
+    line: 'rocket 03-03',
+    style: 'text-white font-semibold text-xl text-blue-light'
   }
 ];
