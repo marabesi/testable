4a632a2307215e9589781b38a1b72c92cb6daf05
marabesi <matheus.marabesi@gmail.com>
2019-08-05 23:28:32 +0200

    created index.spec.js file

---
+++ src/index.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+
+describe('Application root', () => {
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    div.id = 'root';
+    document.body.appendChild(div);
+    require('./index.js');
+    expect(ReactDOM.render).toHaveBeenCalled();
+  });
+});
