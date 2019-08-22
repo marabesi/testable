c8e6c1d3822e6f96643387a311f4127c38768a85
marabesi <matheus.marabesi@gmail.com>
2019-08-22 18:16:06 +0200

    added base image 64 component test case

---
+++ src/components/base64image/Base64Image.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import Base64Image from './Base64Image';
+import Emitter, { TRACKING } from '../../emitter/Emitter';
+
+describe('base64image behavior', () => {
+  afterEach(() => {
+    Emitter.removeAllListeners(TRACKING);
+  });
+
+  test('emit track event on click', () => {
+    const listener = jest.fn();
+    Emitter.addListener(TRACKING, listener);
+    const wrapper = mount(<Base64Image />);
+    wrapper.find('img').simulate('click');
+    expect(listener).toBeCalled();
+  });
+});
