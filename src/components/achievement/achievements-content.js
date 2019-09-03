a210ed47a64f3f51edbf50c10cefcecb9057b587
marabesi <matheus.marabesi@gmail.com>
2019-09-03 22:01:27 +0200

    prevent props validation warning (achievements component)

--- src/components/achievement/achievements-content.js
+++ src/components/achievement/achievements-content.js
@@ -1,34 +1,26 @@
 export default [
   {
     title: 'Desafio aceito',
-    description: [
-      'Vamos construir um foguete!'
-    ],
+    description: 'Vamos construir um foguete!',
     level: 2,
     active: false
   },
   {
     title: 'Tutorial completo',
-    description: [
-      'Função de soma criada com sucesso!'
-    ],
+    description: 'Função de soma criada com sucesso!',
     level: 3,
     active: false
   },
   {
     title: 'Testar depois de fazer o código',
     level: 4,
-    description: [
-      'Não deixe tudo em uma única função',
-    ],
+    description: 'Não deixe tudo em uma única função',
     active: false
   },
   {
     title: 'Teste unitário',
     level: 5,
-    description: [
-      'Previne possíveis mudanças indesejadas e testa pequenas unidades de código como as funções',
-    ],
+    description: 'Previne possíveis mudanças indesejadas e testa pequenas unidades de código como as funções',
     active: false
   }
 ];
