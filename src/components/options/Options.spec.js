f56ee3cee7caabb1f9ba04e4a29ec1a5412984be
marabesi <matheus.marabesi@gmail.com>
2019-09-04 21:54:57 +0200

    added test case with redux-mock-store (options component)

--- src/components/options/Options.spec.js
+++ src/components/options/Options.spec.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import { Options } from './Options';
+import {shallow, mount} from 'enzyme';
+import { IntlProvider } from 'react-intl';
+import configureMockStore from 'redux-mock-store';
+import Options from './Options';
+import { Options as OptionsWithoutStore } from './Options';
+import { SET_LOCALE } from '../../actions/localeAction';
+
+const mockStore = configureMockStore();
 
 describe('options menu component', () => {
 
   test('should show up select to chose the desired language', () => {
-    const wrapper = shallow(<Options />);
+    const wrapper = shallow(<OptionsWithoutStore />);
     expect(wrapper.find('select').exists()).toBeTruthy();
   });
 
   test('Portuguese should be the language by default', () => {
-    const wrapper = shallow(<Options />);
+    const wrapper = shallow(<OptionsWithoutStore />);
     expect(wrapper.find('select').props().value).toEqual('pt-br');
   });
 
   test('switch Portuguese to English', () => {
     const setLocale = jest.fn();
-    const wrapper = shallow(<Options setLocale={setLocale} />);
+    const wrapper = shallow(<OptionsWithoutStore setLocale={setLocale} />);
 
     wrapper.find('select').simulate('change', {target: { value : 'en'}});
 
     expect(setLocale).toBeCalledWith('en');
+  });
+
+  test('dispatch action on change selected language', () => {
+    const store = mockStore({});
+    const wrapper = mount(
+      <IntlProvider locale={'en'}>
+        <Options store={store} />
+      </IntlProvider>
+    );
+
+    wrapper.find('select').simulate('change', {target: { value : 'en'}});
+    const actions = store.getActions();
+
+    expect(actions).toEqual([ { 'payload': 'en', 'type': SET_LOCALE } ]);
   });
 });
