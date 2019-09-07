82b6e24c41e0c2b204fc9ba32fd983f12decd91f
marabesi <matheus.marabesi@gmail.com>
2019-09-07 20:09:26 +0200

    updated assets

--- src/components/buggy/SvgBuggy.js
+++ src/components/buggy/SvgBuggy.js
@@ -5,19 +5,19 @@ import Base64Image from '../base64image/Base64Image';
  * @param {object} props
  */
 const SvgBuggy = props => (
-  <Base64Image image="testable.buggy.svg" alt="buggy" {...props}/>
+  <Base64Image image="testable.buggy-right.png" alt="buggy" {...props}/>
 );
 
 export const SvgBuggyLeft = props => (
-  <Base64Image image="testable.buggy.png" alt="buggy" {...props}/>
+  <Base64Image image="testable.buggy-left.png" alt="buggy" {...props}/>
 );
 
 export const SvgBuggyBug = props => (
-  <Base64Image image="testable.buggy-bug.svg" alt="buggy bug!!!" {...props}/>
+  <Base64Image image="testable.buggy-bug.png" alt="buggy bug!!!" {...props}/>
 );
 
 export const SvgBuggySleepy = props => (
-  <Base64Image image="testable.buggy-zzz.svg" alt="buggy zzz" {...props}/>
+  <Base64Image image="testable.buggy-zzz.png" alt="buggy zzz" {...props}/>
 );
 
 export default SvgBuggy;
