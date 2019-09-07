6babcfec15bab9c16f61c9ff5d2a6964912ac432
marabesi <matheus.marabesi@gmail.com>
2019-09-08 00:29:47 +0200

    added button on hover (Button component)

--- src/components/scene-manager/Button.js
+++ src/components/scene-manager/Button.js
@@ -6,7 +6,7 @@ import './button.scss';
 export default class Button extends Component {
 
   render() {
-    const className = `button focus:outline-none ${this.props.className ? this.props.className: ''}`;
+    const className = `button hover:bg-testable-pink-dark focus:outline-none ${this.props.className ? this.props.className: ''}`;
     return (
       <button className={className} onClick={this.props.onClick}>{this.props.description}</button>
     );
