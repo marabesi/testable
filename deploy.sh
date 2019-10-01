a3f8d54811372cbb2466c28d21a59e641e1a7cb5
marabesi <matheus.marabesi@gmail.com>
2019-10-01 22:23:16 +0200

    added cypress video and screenshots to ghpages

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,8 @@ git config user.name "$GIT_NAME" && \
 git config user.email "$GIT_EMAIL" && \
 git add -f build && \
 git add -f cypress && \
+git add -f cypress/screenshots && \
+git add -f cypress/videos && \
 git commit -m 'build' && \
 git push --force $remote_repo master:$remote_branch && \
 rm -fr .git
