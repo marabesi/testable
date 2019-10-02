f7988827eb3df8bd943f5709ad8fb7f9ceca9f49
marabesi <matheus.marabesi@gmail.com>
2019-10-02 13:24:27 +0200

    removed cypress folder from deploy

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,6 @@ git init && \
 git config user.name "$GIT_NAME" && \
 git config user.email "$GIT_EMAIL" && \
 git add -f build && \
-git add -f cypress && \
-git add -f cypress/screenshots && \
-git add -f cypress/videos && \
 git commit -m 'build' && \
 git push --force $remote_repo master:$remote_branch && \
 rm -fr .git
