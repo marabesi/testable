c980bdd43aa118fca6d9ec51d01b078e1963a52d
marabesi <matheus.marabesi@gmail.com>
2019-10-03 08:44:57 +0200

    replaced duplicated code by hoc (SceneContentManager)

--- src/pages/tdd-end/TddEnd.js
+++ src/pages/tdd-end/TddEnd.js
@@ -1,42 +1,8 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import content from './tdd-end-content.json';
-import SceneManager from '../../components/scene-manager/SceneManager';
-import Emitter, { LEVEL_UP } from '../../emitter/Emitter';
+import WrappedSceneContentManager from '../../components/scene-manager/SceneContentManager';
 
-export class TddEnd extends Component {
-
-  state = {
-    redirect: false
-  };
-
-  handleLastScene = () => {
-    Emitter.emit(LEVEL_UP);
-
-    setTimeout(() => {
-      this.setState({
-        redirect: true
-      });
-    }, 1000);
-  }
-
-  render() {
-    if (this.state.redirect) {
-      return (
-        <Redirect to={{
-          pathname: '/rocket-01',
-        }} />
-      );
-    }
-    return (
-      <SceneManager
-        identifier="tdd-end"
-        content={content}
-        handleLastScene={this.handleLastScene}
-      />
-    );
-  }
-}
-
-export default connect()(TddEnd);
+export default WrappedSceneContentManager(
+  'tdd-end',
+  content,
+  'rocket-01'
+);
--- src/pages/tdd-end/TddEnd.spec.js
+++ src/pages/tdd-end/TddEnd.spec.js
@@ -1,66 +1,10 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {mount, shallow} from 'enzyme';
-import {TddEnd} from './TddEnd';
-import Emmiter, {LEVEL_UP} from '../../emitter/Emitter';
+import {mount } from 'enzyme';
+import TddEnd from './TddEnd';
 
 describe('tdd end page', () => {
-
-  let wrappedComponent = null;
-
-  beforeEach(() => {
-    Emmiter.removeAllListeners(LEVEL_UP);
-    wrappedComponent = shallow(
-      <BrowserRouter>
-        <TddEnd onLoading={() => false} />
-      </BrowserRouter>
-    );
-  });
-
-  afterEach(() => {
-    Emmiter.removeAllListeners(LEVEL_UP);
-    wrappedComponent = null;
-  });
-  
   test('render tdd end page', () => {
     const wrapper = mount(<TddEnd />);
-    expect(wrapper.find('SceneManager').length).toBe(1);
-  });
-
-  describe('handles the last scene', () => {
-    test('should level up', () => {
-      const callback = jest.fn();
-      Emmiter.addListener(LEVEL_UP, callback);
-
-      wrappedComponent.find('TddEnd').dive().instance().handleLastScene();
-
-      expect(callback).toBeCalled();
-    });
-
-    test('should redirect to rocket 01', () => {
-      const wrappedPage = wrappedComponent.find('TddEnd').dive();
-      wrappedPage.instance().setState({
-        redirect: true
-      });
-      const redirect = wrappedPage.find('Redirect');
-      expect(redirect.length).toBe(1);
-      expect(redirect.prop('to').pathname).toEqual('/rocket-01');
-    });
-
-    test('should redirect when done', done => {
-      const wrappedWithRouter = mount(
-        <BrowserRouter>
-          <TddEnd />
-        </BrowserRouter>
-      );
-
-      wrappedWithRouter.find('TddEnd').instance().handleLastScene();
-
-      setTimeout(() => {
-        wrappedWithRouter.update();
-        expect(wrappedWithRouter.find('Redirect').length).toBe(1);
-        done();
-      }, 1400);
-    });
+    expect(wrapper.find('SceneContentManager').length).toBe(1);
   });
 });
