362f81f4ede824aefc7b4f3910ff80419271c7ec
marabesi <matheus.marabesi@gmail.com>
2019-10-07 08:34:04 +0200

    renamed identifier (challenge 03 - 01)

--- src/pages/rocket-03/Challenge03_01.js
+++ src/pages/rocket-03/Challenge03_01.js
@@ -3,7 +3,7 @@ import content from './challenge03_01-content.json';
 import WrappedSceneContentManager from '../../components/scene-manager/SceneContentManager';
 
 export default WrappedSceneContentManager(
-  'Challenge03_01',
+  'challenge03_01',
   content,
   'rocket-02-02'
 );
