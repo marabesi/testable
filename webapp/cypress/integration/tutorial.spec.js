bdba73deeffdb93eb6347ead5b1ae44cd8312a2b
marabesi <matheus.marabesi@gmail.com>
2019-10-12 20:10:01 +0200

    improved tutorial cypress spec

--- webapp/cypress/integration/tutorial.spec.js
+++ webapp/cypress/integration/tutorial.spec.js
@@ -28,56 +28,59 @@ context('tutorial', () => {
   });
 
   it('should follow interface instructions', () => {
-    cy.wait(5000);
-    cy.get('.next').should('be.visible');
+    cy.get('.next').should('be.visible', { timeout: 5000 });
     cy.get('.next').click();
 
-    for (let i = 0; i < 9; i++) {
-      cy.wait(1000);
-      cy.get('.introjs-nextbutton').should('be.visible');
+    for (let i = 0; i < 11; i++) {
+      cy.get('.introjs-nextbutton').should('be.visible', { timeout: 1000 });
       cy.get('.introjs-nextbutton').click();
+      cy.get('.header').scrollIntoView();
     }
 
     cy.get('.introjs-donebutton').should('be.visible');
     cy.get('.introjs-donebutton').click();
 
-    cy.wait(1000);
+    cy.get('.header').scrollIntoView();
 
-    cy.get('.next').should('be.visible');
+    cy.get('.next').should('be.visible', { timeout: 3000 });
   });
 
   it('should follow guide instructions', () => {
-    cy.wait(5000);
-    cy.get('.next').should('be.visible');
+    cy.get('.next').should('be.visible', { timeout: 5000 });
     cy.get('.next').click();
 
     // skip interface instruction
     cy.get('.introjs-skipbutton').click();
 
-    cy.wait(2000);
-    cy.get('.next').should('be.visible');
+    cy.get('.next').should('be.visible', { timeout: 2000 });
     cy.get('.next').click();
+    cy.get('.header').scrollIntoView();
 
-    cy.wait(2000);
-    cy.get('.next').should('not.exist');
+    cy.get('.next').should('not.exist', { timeout: 2000 });
+    cy.get('.header').scrollIntoView();
   });
 
   it('should not allow user to type code before reaching the correct step', () => {
-    cy.wait(1000);
-    cy.get('.editor-0 .CodeMirror textarea').type('var a = 1;', { force: true });
-    cy.get('.editor-0.forbidden').should('be.visible');
+    cy.get('.editor-0 .CodeMirror textarea', { timeout: 4000 }).type('var a = 1;', { force: true });
+    cy.get('.editor-0.forbidden').should('be.visible', { timeout: 1000 });
   });
 
   it('should warn the user on invalid code', () => {
-    cy.wait(5000);
+    cy.get('.next').should('be.visible', { timeout: 3000 });
     cy.get('.next').click();
+
     // skip interface instruction
-    cy.get('.introjs-skipbutton').click();
-    cy.wait(2000);
-    cy.get('.next').click();
-    cy.wait(10000);
-    cy.get('.editor-0 .CodeMirror textarea').type('my code', { force: true });
+    cy.get('.introjs-skipbutton').click({ timeout: 3000 });
+
+    cy.get('.next').click({ timeout: 2000 });
+
+    cy.contains('.type-wrap span > .text-white', '!', { timeout: 20000 });
+
+    cy.get('.editor-0.forbidden').should('not.exist', { timeout: 2000 });
     cy.wait(1000);
+
+    cy.get('.editor-0 .CodeMirror textarea').type('my code', { force: true });
+    cy.wait(500);
     cy.get('.text-red').contains('Uncaught SyntaxError: Unexpected identifier');
   });
 });
