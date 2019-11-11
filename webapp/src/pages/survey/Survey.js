26a823b067ad4f9b81a70c55502f382d04e94e35
marabesi <matheus.marabesi@gmail.com>
2019-11-11 23:37:38 +0100

    replaced class survey page by function

--- webapp/src/pages/survey/Survey.js
+++ webapp/src/pages/survey/Survey.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SurveyModal from '../../components/survey/Survey';
 
-export default class Survey extends Component {
-  render() {
-    return (
-      <SurveyModal skip={true} />
-    );
-  }
-}
+const SurveyPage = () => <SurveyModal skip={true} />;
+
+export default SurveyPage;
