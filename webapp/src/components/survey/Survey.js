9526d8d789a3dd686c29777c197424f6379c04d1
marabesi <matheus.marabesi@gmail.com>
2019-11-11 23:05:43 +0100

    created survey component

---
+++ webapp/src/components/survey/Survey.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Button from '../scene-manager/Button';
+import Emitter, { LEVEL_UP } from '../../emitter/Emitter';
+
+import '../../scss/shake-horizontal.scss';
+import './survey.scss';
+
+/* eslint-disable-next-line */
+const survey = process.env.REACT_APP_SURVEY_URL || '';
+
+const mapStateToProps = state => ({
+  user: state.userReducer.user,
+});
+
+export class Survey extends Component {
+
+  state = {
+    surveyUrl: '',
+    loading: true,
+    buttonDescription: 'Responder o questionário depois, quero descobrir o que é TDD!'
+  }
+
+  componentDidMount() {
+    if (survey) {
+      const surveyUrl = survey.replace('{id}', this.props.user.uid);
+      this.setState({
+        surveyUrl: surveyUrl
+      });
+    }
+  }
+
+  onSurveyLoaded = () => {
+    this.setState({ loading: false });
+  }
+
+  skipSurvey = () => {
+    Emitter.emit(LEVEL_UP);
+  }
+
+  render() {
+    if (this.props.user.uid && survey) {
+      return (
+        <div className={`w-full ${this.props.className}`}>
+          {
+            this.state.loading &&
+            <div className="flex justify-center items-center text-white">
+              <h1>Carregando questionário...</h1>
+            </div>
+          }
+
+          <iframe
+            style={{ height: '65vh', width: '100%' }}
+            src={this.state.surveyUrl}
+            title="survey form"
+            onLoad={this.onSurveyLoaded}
+          />
+
+          {
+            !this.state.loading && this.props.skip &&
+            <Button
+              className="block mt-5 m-auto"
+              description={this.state.buttonDescription}
+              onClick={this.skipSurvey}
+            />
+          }
+        </div>
+      );
+    }
+
+    return (
+      <div className="flex justify-center items-center text-white">
+        <h1 className="shake-horizontal">Ocorreu um erro ao carregar o questionário</h1>
+      </div>
+    );
+  }
+}
+
+Survey.propTypes = {
+  user: PropTypes.object,
+  skip: PropTypes.bool
+};
+
+Survey.defaultProps = {
+  user: {},
+  skip: false
+};
+
+export default connect(mapStateToProps)(Survey);
---
+++ webapp/src/components/survey/Survey.spec.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { Survey } from './Survey';
+import { auth } from '../../pages/login/Auth';
+import Button from '../scene-manager/Button';
+
+describe('Survey page', () => {
+
+  test('should show up message when survey url is not defined',() => {
+    const wrapper = mount(<Survey user={{ uid: 'uhiuqwqw-k-woqk-wq--qw' }} />);
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Carregando questionário...');
+  });
+
+  test('should not load survey when user id is missing', () => {
+    const wrapper = mount(<Survey user={{ }} />);
+    const text = wrapper.find('h1').text();
+    expect(text).toEqual('Ocorreu um erro ao carregar o questionário');
+  });
+  test('load survey passing user id as a parameter in the query string', () => {
+    const user = { uid: 'uhiuqwqw-k-woqk-wq--qw' };
+
+    const wrapper = mount(<Survey user={user} />);
+    const url = wrapper.find('iframe').prop('src');
+    expect(url.includes(auth.user.uid)).toBe(true);
+  });
+
+  test('should not up button when it is loading', () => {
+    const user = { uid: 'uhiuqwqw-k-woqk-wq--qw' };
+
+    const wrapper = mount(<Survey user={user} />);
+    expect(wrapper.find(Button).length).toBe(0);
+  });
+
+  test('should not up button when skip prop is not set', () => {
+    const user = { uid: 'uhiuqwqw-k-woqk-wq--qw' };
+
+    const wrapper = mount(<Survey user={user} />);
+    expect(wrapper.find(Button).length).toBe(0);
+  });
+
+  test('show up button when loading is done and skip prop is true', () => {
+    const user = { uid: 'uhiuqwqw-k-woqk-wq--qw' };
+
+    const wrapper = mount(<Survey user={user} skip={true} />);
+    wrapper.setState({
+      loading: false
+    });
+    expect(wrapper.find(Button).length).toBe(1);
+  });
+});
