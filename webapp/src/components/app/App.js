e468fef434a609f57c410213f12c5caf453c7ad0
marabesi <matheus.marabesi@gmail.com>
2020-02-18 22:03:17 +0100

    feat(react code splitting): implemented suspense and error boundary

--- webapp/src/components/app/App.js
+++ webapp/src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { Component  } from 'react';
+import React, { Component, Suspense, lazy  } from 'react';
 import { connect } from 'react-redux';
 import { Route } from 'react-router-dom';
 import { AnimatedSwitch } from 'react-router-transition';
@@ -9,29 +9,31 @@ import ProtectedRoute from '../../pages/login/router/ProtectedRoute';
 import { messages } from '../../constants/locale';
 import Queue from '../../queue/queue';
 import Loading from '../loading/Loading';
+import NotFound from '../../pages/notfound/NotFound';
 import Sidebar from '../sidebar/Sidebar';
-import Introduction from '../../pages/introduction/Introduction';
 import Login from '../../pages/login/Login';
 import Emitter, { TRACKING } from '../../emitter/Emitter';
-import Tutorial from '../../pages/tutorial/Tutorial';
-import NotFound from '../../pages/notfound/NotFound';
-import TutorialEnd from '../../pages/tutorial-end/TutorialEnd';
-import UnitTestingIntro from '../../pages/unit-testing-intro/UnitTestingIntro';
-import UnitTesting from '../../pages/unit-testing/UnitTesting';
-import UnitTestingEnd from '../../pages/unit-testing-end/UnitTestingEnd';
-import Rocket01 from '../../pages/rocket-01/Rocket01';
-import Rocket02 from '../../pages/rocket-02/Rocket02';
-import Rocket03 from '../../pages/rocket-03/Rocket03';
-import Challenge03_01 from '../../pages/rocket-03/Challenge03_01';
-import Challenge03_02 from '../../pages/rocket-03/Challenge03_02';
-import CompletedIntro from '../../pages/completed-intro/CompletedIntro';
-import CompletedEnd from '../../pages/completed-end/CompletedEnd';
-import Survey from '../../pages/survey/Survey';
-import TddIntro from '../../pages/tdd-intro/TddIntro';
-import Tdd from '../../pages/tdd/Tdd';
+import ErrorBoundary from './ErrorBoundary';
 import { auth } from '../../pages/login/Auth';
 
 import './app.scss';
+
+const Introduction = lazy(() => import('../../pages/introduction/Introduction'));
+const Tutorial = lazy(() => import('../../pages/tutorial/Tutorial'));
+const TutorialEnd = lazy(() => import('../../pages/tutorial-end/TutorialEnd'));
+const UnitTestingIntro = lazy(() => import('../../pages/unit-testing-intro/UnitTestingIntro'));
+const UnitTesting = lazy(() => import('../../pages/unit-testing/UnitTesting'));
+const UnitTestingEnd = lazy(() => import('../../pages/unit-testing-end/UnitTestingEnd'));
+const Rocket01 = lazy(() => import('../../pages/rocket-01/Rocket01'));
+const Rocket02 = lazy(() => import('../../pages/rocket-02/Rocket02'));
+const Rocket03 = lazy(() => import('../../pages/rocket-03/Rocket03'));
+const Challenge03_01 = lazy(() => import('../../pages/rocket-03/Challenge03_01'));
+const Challenge03_02 = lazy(() => import('../../pages/rocket-03/Challenge03_02'));
+const CompletedIntro = lazy(() => import('../../pages/completed-intro/CompletedIntro'));
+const CompletedEnd = lazy(() => import('../../pages/completed-end/CompletedEnd'));
+const Survey = lazy(() => import('../../pages/survey/Survey'));
+const TddIntro = lazy(() => import('../../pages/tdd-intro/TddIntro'));
+const Tdd = lazy(() => import('../../pages/tdd/Tdd'));
 
 const isDebug = process.env.REACT_APP_DEBUG || false;
 const queue = new Queue();
@@ -51,9 +53,6 @@ const assets = [
   '/assets/mp3/keyboard.mp3',
 ];
 
-/**
- * @param {object} state
- */
 const mapStateToProps = state => ({
   locale: state.localeReducer.locale,
 });
@@ -97,36 +96,40 @@ export class App extends Component {
     const { locale } = this.props;
 
     return (
-      <IntlProvider locale={locale} messages={messages[locale]}>
-        <Sidebar>
-          <AnimatedSwitch
-            atEnter={bounceTransition.atEnter}
-            atLeave={bounceTransition.atLeave}
-            atActive={bounceTransition.atActive}
-            mapStyles={mapStyles}
-            className="App"
-          >
-            <Route exact path="/" component={Login} />
-            <ProtectedRoute path="/intro" component={Introduction} />
-            <ProtectedRoute path="/tutorial" component={Tutorial} />
-            <ProtectedRoute path="/tutorial-end" component={TutorialEnd} />
-            <ProtectedRoute path="/unit-testing-intro" component={UnitTestingIntro} />
-            <ProtectedRoute path="/unit-testing" component={UnitTesting} />
-            <ProtectedRoute path="/unit-testing-end" component={UnitTestingEnd} />
-            <ProtectedRoute path="/rocket-01" component={Rocket01} />
-            <ProtectedRoute path="/rocket-02" component={Rocket02} />
-            <ProtectedRoute path="/rocket-03" component={Rocket03} />
-            <ProtectedRoute path="/rocket-03-01" component={Challenge03_01} />
-            <ProtectedRoute path="/rocket-03-02" component={Challenge03_02} />
-            <ProtectedRoute path="/completed-intro" component={CompletedIntro} />
-            <ProtectedRoute path="/completed-end" component={CompletedEnd} />
-            <ProtectedRoute path="/survey" component={Survey} />
-            <ProtectedRoute path="/tdd-intro" component={TddIntro} />
-            <ProtectedRoute path="/tdd" component={Tdd} />
-            <Route path="*" component={NotFound} />
-          </AnimatedSwitch>
-        </Sidebar>
-      </IntlProvider>
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <IntlProvider locale={locale} messages={messages[locale]}>
+            <Sidebar>
+              <AnimatedSwitch
+                atEnter={bounceTransition.atEnter}
+                atLeave={bounceTransition.atLeave}
+                atActive={bounceTransition.atActive}
+                mapStyles={mapStyles}
+                className="App"
+              >
+                <Route exact path="/" component={Login} />
+                <ProtectedRoute path="/intro" component={Introduction} />
+                <ProtectedRoute path="/tutorial" component={Tutorial} />
+                <ProtectedRoute path="/tutorial-end" component={TutorialEnd} />
+                <ProtectedRoute path="/unit-testing-intro" component={UnitTestingIntro} />
+                <ProtectedRoute path="/unit-testing" component={UnitTesting} />
+                <ProtectedRoute path="/unit-testing-end" component={UnitTestingEnd} />
+                <ProtectedRoute path="/rocket-01" component={Rocket01} />
+                <ProtectedRoute path="/rocket-02" component={Rocket02} />
+                <ProtectedRoute path="/rocket-03" component={Rocket03} />
+                <ProtectedRoute path="/rocket-03-01" component={Challenge03_01} />
+                <ProtectedRoute path="/rocket-03-02" component={Challenge03_02} />
+                <ProtectedRoute path="/completed-intro" component={CompletedIntro} />
+                <ProtectedRoute path="/completed-end" component={CompletedEnd} />
+                <ProtectedRoute path="/survey" component={Survey} />
+                <ProtectedRoute path="/tdd-intro" component={TddIntro} />
+                <ProtectedRoute path="/tdd" component={Tdd} />
+                <Route path="*" component={NotFound} />
+              </AnimatedSwitch>
+            </Sidebar>
+          </IntlProvider>
+        </Suspense>
+      </ErrorBoundary>
     );
   }
 }
---
+++ webapp/src/components/app/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
