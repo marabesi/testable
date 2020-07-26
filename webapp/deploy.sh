78175512ee666cf89b576242032674558386e21f
marabesi <matheus.marabesi@gmail.com>
2020-07-26 15:44:14 +0200

    ci: removed build folder, instead push assets directly

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -4,10 +4,11 @@ remote_repo=${REPO_URL:-`git config remote.origin.url`}
 remote_branch="gh-pages"
 
 rm -fr .git && \
+cd build && \
 git init && \
 git config user.name "$GIT_NAME" && \
 git config user.email "$GIT_EMAIL" && \
-git add -f build && \
+git add -f . && \
 git commit -m 'build' && \
 git push --force $remote_repo master:$remote_branch && \
 rm -fr .git
