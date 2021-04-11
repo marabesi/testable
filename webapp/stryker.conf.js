00de55ca99d44dbe28e32c1afcd5802ee5dd960a
Matheus Marabesi <matheus.marabesi@gmail.com>
2021-04-11 18:17:35 +0200

    ci: setting up stryker

--- webapp/stryker.conf.js
+++ webapp/stryker.conf.js
@@ -2,14 +2,20 @@
  * @type {import('@stryker-mutator/api/core').StrykerOptions}
  */
 module.exports = {
-  _comment:
-    'This config was generated using \'stryker init\'. Please see the guide for more information: https://stryker-mutator.io/docs/stryker/guides/react',
   testRunner: 'jest',
-  reporters: ['progress', 'clear-text', 'html'],
+  reporters: ['progress', 'html'],
+  htmlReporter: {
+    baseDir: 'public/stryker'
+  },
+  timeoutMS: 1500,
   coverageAnalysis: 'off',
   jest: {
     projectType: 'create-react-app',
   },
-  mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)', '!src/__test__/**/*@(.test|.spec|Spec).ts?(x)'],
+  mutate: ['src/**/*.ts?(x)', '!src/**/*@(.test|.spec|Spec).ts?(x)', '!src/__test__/**'],
   mutator: 'typescript',
+  tempDirName: '.stryker-tmp',
+  files: [
+    '**',
+  ],
 };
